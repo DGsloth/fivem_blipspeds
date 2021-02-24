69aa673042f4877e80ad9cf4f66deba57b7c38be
James <edwardsjames@live.com>
2021-02-24 13:48:43 -0500

    UPDATE THE UPDATER

--- blipspeds/update.lua
+++ blipspeds/update.lua
@@ -3,8 +3,8 @@ local addonName = 'fivem_blipspeds'
 PerformHttpRequest('https://raw.githubusercontent.com/'..userName..'/'..addonName..'/main/version.txt', function(err, serverVersion, headers)
     local version = '1.1'
     if string.find(serverVersion, version) == nil then
-        print('^4ADDON: ^3', GetCurrentResourceName(), ' ^7[^1OUTDATED^7] - https://github.com/'..userName..'/'..addonName)
+        print('^4ADDON: ^3', GetCurrentResourceName(), '^7[^3'..addonName..'^7]', '^7[^1OUTDATED^7] - https://github.com/'..userName..'/'..addonName)
     else
-        print('^4ADDON: ^3', GetCurrentResourceName(), ' ^7[^2UPDATED^7]')
+        print('^4ADDON: ^3', GetCurrentResourceName(), '^7[^3'..addonName..'^7]', '[^2UP-TO-DATE^7]')
     end
 end)
